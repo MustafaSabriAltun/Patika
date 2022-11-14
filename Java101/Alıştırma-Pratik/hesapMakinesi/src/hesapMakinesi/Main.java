8a7522c350b88efdc0f166535a0f97ca08e7f545
MustafaSabriAltun <77845838+MustafaSabriAltun@users.noreply.github.com>
2022-11-14 20:48:20 +0300

    www.patika.dev
    www.patika.dev

--- Java101/Alıştırma-Pratik/hesapMakinesi/src/hesapMakinesi/Main.java
+++ Java101/Alıştırma-Pratik/hesapMakinesi/src/hesapMakinesi/Main.java
@@ -5,19 +5,16 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner ınput1 = new Scanner(System.in);
-		Scanner ınput2 = new Scanner(System.in);
-		Scanner x = new Scanner(System.in);
+		Scanner ınput = new Scanner(System.in);
 		double sonuc=0, sayı1, sayı2;
 		String ıslem;
 		System.out.println("bir sayı giriniz");
-		sayı1=ınput1.nextDouble();
+		sayı1=ınput.nextDouble();
 		System.out.println("bir işlem giriniz");
-		ıslem= x.nextLine();
+		ıslem= ınput.next();
 		System.out.println("bir sayı giriniz");
-		sayı2=ınput2.nextDouble();
-		
-		
+		sayı2=ınput.nextDouble();
+
 		switch (ıslem) {
 		case "+":
 			sonuc=sayı1 + sayı2;
@@ -31,7 +28,6 @@ public class Main {
 		case "*":
 			sonuc=sayı1*sayı2;
 			break;
-
 		default:
 			System.out.println("Lütfen Yapılacak işlemi giriniz.");
 			break;
