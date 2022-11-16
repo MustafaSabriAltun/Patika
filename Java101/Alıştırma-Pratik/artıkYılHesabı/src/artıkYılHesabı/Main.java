99e92c0f81a68c154e1ef32958c8b7315e5f0805
MustafaSabriAltun <77845838+MustafaSabriAltun@users.noreply.github.com>
2022-11-16 14:42:26 +0300

    www.patika.dev
    www.patika.dev

---
+++ Java101/Alıştırma-Pratik/artıkYılHesabı/src/artıkYılHesabı/Main.java
@@ -0,0 +1,28 @@
+package artıkYılHesabı;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int age = 0;
+
+		System.out.println("Yıl giriniz : ");
+		age = input.nextInt();
+
+		if (age % 100 == 0) {
+			if (age % 400 == 0) {
+				System.out.println(age + " ARTIK YILDIR");
+			} else {
+				System.out.println(age + " artık yıl değildir.(400'ün katı olmadığı için)");
+			}
+		} else if (age % 4 == 0) {
+			System.out.println(age + " ARTIK YILDIR");
+		} else {
+			System.out.println(age + " artık yıl değildir.");
+		}
+
+	}
+
+}
