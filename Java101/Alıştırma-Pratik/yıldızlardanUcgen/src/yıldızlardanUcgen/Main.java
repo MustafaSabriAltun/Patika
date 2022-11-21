89955758135796127146fd446b5c33192eb6f50b
MustafaSabriAltun <77845838+MustafaSabriAltun@users.noreply.github.com>
2022-11-21 19:11:23 +0300

    www.patika.dev
    www.patika.dev

---
+++ Java101/Alıştırma-Pratik/yıldızlardanUcgen/src/yıldızlardanUcgen/Main.java
@@ -0,0 +1,36 @@
+package yıldızlardanUcgen;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n;
+		System.out.println("bir sayı giriniz.");
+		n=input.nextInt();
+		
+		for (int i = 1; i<=n; i++) {
+			for (int j = 0; j<(n-i) ; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k<=(2*i-1); k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = n; i>=0; i--) {
+			for (int j = 0; j<(n-i) ; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k<=(2*i-1); k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
