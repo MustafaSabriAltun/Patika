653774720e97bc92160874a86b5b7ed2dbf74e5b
MustafaSabriAltun <77845838+MustafaSabriAltun@users.noreply.github.com>
2022-11-29 15:59:28 +0300

    www.patika.dev
    www.patika.dev

---
+++ Java101/Alıştırma-Pratik/asalSayıBulma/src/asalSayıBulma/Main.java
@@ -0,0 +1,23 @@
+package asalSayıBulma;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		for (int i = 2; i < 100; i++) {
+			boolean prime = true;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					prime = false;
+
+				}
+			}
+			if (prime) {
+				System.out.println(i + "  ");
+			}
+
+		}
+
+	}
+
+}
